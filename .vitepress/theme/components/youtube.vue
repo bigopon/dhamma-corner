cac052e9390696003545af0dc238552e83d90ec7
bigopon <9994529+bigopon@users.noreply.github.com>
2023-05-27 04:36:37 +0000

    fix: add iframe maxwidth

--- .vitepress/theme/components/youtube.vue
+++ .vitepress/theme/components/youtube.vue
@@ -7,6 +7,7 @@
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen
+    style="max-width: 100%"
   />
 </template>
 
